2247a0d09d6874f4b1222b4e620b9035dd974c35
David Pardo <dajupaco@gmail.com>
2025-09-05 22:03:01 +0200

    chore: first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Getting Started
+
+Web socket to run the chat app: Parche Chat
+
+First, run the server:
+
+```bash
+npx nodemon server.js
+```
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { Server } = require('socket.io');
+
+const app = express();
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+  console.log(`Parche chat socket running on port ${port}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+let onlineUsers = new Set();
+
+io.on('connection', (socket) => {
+  socket.on('join', (userId) => {
+    if (!socket.rooms.has(userId)) {
+      socket.join(userId);
+      if (!onlineUsers.has(userId)) {
+        onlineUsers.add(userId);
+      }
+    }
+    onlineUsers.forEach((user) => {
+      io.to(user).emit('online-users-updated', Array.from(onlineUsers));
+    });
+  });
+
+  socket.on('logout', (userId) => {
+    socket.leave(userId);
+    onlineUsers.delete(userId);
+    onlineUsers.forEach((user) => {
+      io.to(user).emit('online-users-updated', Array.from(onlineUsers));
+    });
+  });
+});
+
+app.get('/', (req, res) => {
+  return res.send('Server is running');
+});
